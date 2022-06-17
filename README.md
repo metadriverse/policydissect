3f9c72ad6c067657ff7e788396084fb50354c9db
Quanyi Li <quanyili0057@gmail.com>
2022-06-17 17:59:00 +0800

    Release quadrupedal (#4)
    * quadrupedal ready
    
    * legged robot ready
    
    * update readme
    
    * control legged

--- README.md
+++ README.md
@@ -11,13 +11,12 @@ Official implementation of the paper: **Human-AI Shared Control via Frequency-ba
 
 Currently, we provide some interactive neural controllers enabled by *Policy Dissection*. 
 The policy dissection method and training code will be updated soon.
-Please run ```python policydissect/play/play_[env].py``` to play with these neural controllers.
 
 
 **Environments**:
 
 - [x] MetaDrive
-- [ ] Pybullet-Quadrupedal Robot
+- [x] Pybullet-Quadrupedal Robot (Forked from: https://github.com/Mehooz/vision4leg.git)
 - [ ] Gym-Walker
 - [ ] Gym-BipedalWalker
 - [ ] Gym-Ant
@@ -35,4 +34,18 @@ conda activate policydissect
 # Install basic dependency
 pip install -e .
 ```
+
+## Play 
+### MetaDrive
+Run```python policydissect/play/play_metadriv.py``` to collaborate with the AI driver. 
+Press ```w```,```a```,```s```,```d``` for triggering lane following, left/right lane changing and braking, and
+```r``` for resetting the environment.
+
+### Quadrupedal Robot
+The quadrupedal robot is trained with the code provided by https://github.com/Mehooz/vision4leg.git.
+For playing with legged robot, run```python policydissect/play/play_quadrupedal.py```.
+Press ```w```,```a```,```s```,```d``` for changing the moving direction. and ```r``` for reset.
+Also, you can collaborate with AI and challenge the hard environment consisting of obstacles and challenging terrains by running ```python policydissect/play/play_quadrupedal.py --hard```
+You can change to a different environment by adding ```--seed you_seed_int_type```.
+*tips: Avoiding running too fast!*
 
---
+++ policydissect/play/play_quadrupedal.py
@@ -0,0 +1,85 @@
+import argparse
+import os.path
+import pickle
+
+import keyboard
+import numpy as np
+import pybullet
+
+from policydissect.quadrupedal.vision4leg.get_env import get_env
+from policydissect.utils.legged_utils import params, seed_env
+from policydissect.utils.policy import ppo_inference_torch
+from policydissect.weights import weights_path
+
+legged_robot_command = "Forward"
+
+
+def update_render(env):
+    pos = env.env.env.env.env.env.env._gym_env._robot.GetBasePosition()
+    text_pos = [pos[0] - 0.5, pos[1], pos[2] + 0.5]
+    pybullet.addUserDebugText(legged_robot_command, text_pos, textSize=4, textColorRGB=[1, 0, 0],
+                              replaceItemUniqueId=True)
+
+
+def legged_control(event):
+    global legged_robot_command
+    if event.event_type == "down" and event.name == "w":
+        legged_robot_command = "Forward"
+    elif event.event_type == "down" and event.name == "d":
+        legged_robot_command = "Turn Right"
+    elif event.event_type == "down" and event.name == "a":
+        legged_robot_command = "Turn Left"
+    elif event.event_type == "down" and event.name == "s":
+        legged_robot_command = "Stop"
+    elif event.event_type == "down" and event.name == "r":
+        legged_robot_command = "Reset"
+
+
+LEGGED_MAP = {"Turn Left": {3: [(239, 85)]},
+              "Turn Right": {3: [(239, -75)]},
+              "Stop": {1: [(76, -70)]}}
+
+if __name__ == "__main__":
+
+    keyboard.hook(legged_control)
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--hard', action="store_true")
+    parser.add_argument('--seed', default=3, type=int)
+    args = parser.parse_args()
+    params["env"]["env_build"]["enable_rendering"] = True
+    if args.hard:
+        params["env"]["env_build"]["terrain_type"] = "random_blocks_sparse_and_heightfield"
+        # params["env"]["env_build"]["terrain_type"] = "random_blocks_sparse_thin_wide"
+    else:
+        params["env"]["env_build"]["terrain_type"] = "plane"
+
+    env = get_env(
+        params['env_name'],
+        params['env'])
+
+    pybullet.configureDebugVisualizer(pybullet.COV_ENABLE_WIREFRAME, 0)
+    pybullet.configureDebugVisualizer(pybullet.COV_ENABLE_KEYBOARD_SHORTCUTS, 0)
+    pybullet.configureDebugVisualizer(pybullet.COV_ENABLE_RGB_BUFFER_PREVIEW, 0)
+    pybullet.configureDebugVisualizer(pybullet.COV_ENABLE_DEPTH_BUFFER_PREVIEW, 0)
+    pybullet.configureDebugVisualizer(pybullet.COV_ENABLE_SEGMENTATION_MARK_PREVIEW, 0)
+    pybullet.configureDebugVisualizer(pybullet.COV_ENABLE_GUI, 0)
+    pybullet.configureDebugVisualizer(pybullet.COV_ENABLE_SHADOWS, 0)
+
+    seed = args.seed
+    seed_env(env, seed)
+    pybullet.configureDebugVisualizer(pybullet.COV_ENABLE_GUI, 0, lightPosition=(0, -50, 2))
+    with open(os.path.join(weights_path, "quadrupedal_obs_normalizer.pkl"), 'rb') as f:
+        env._obs_normalizer = pickle.load(f)
+    policy_weights = np.load(os.path.join(weights_path, "quadrupedal.npz"))
+
+    o = env.reset()
+    while True:
+        if legged_robot_command == "Reset":
+            o = env.reset()
+            legged_robot_command = "Forward"
+        action = ppo_inference_torch(policy_weights, o, LEGGED_MAP, legged_robot_command)
+        o, r, d, i = env.step(action)
+        update_render(env)
+        if d:
+            seed_env(env, seed)
+            env.reset()
