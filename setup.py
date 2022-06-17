53f5965f33d111ec2b0cff0cda831758eea5d7c1
lqy0057 <785878978@qq.com>
2022-06-17 15:24:13 +0800

    setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,8 @@ assert sys.version_info.major == 3 and sys.version_info.minor >= 6, "python vers
 setup(
     name="policydissect",
     install_requires=[
-       "metadrive-simulator=0.2.5.1",
-        "keyboard"
+        "metadrive-simulator~=0.2.5.1",
+        "keyboard",
+        "yapf",
     ],
 )
